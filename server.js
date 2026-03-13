ff9bbac3cb16bce483e9099cd7a464e9c46ed7b7
Dilip Reddymalla <reddymaladilip@gmail.com>
2026-03-14 00:55:01 +0530

    removed self api calling

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const fetch = require("node-fetch");
 connectToDB();
 
 const PORT = process.env.PORT || 3000;
-const URL = "https://modern-blog-page-backend.onrender.com/";
 
-setInterval(() => {
-  fetch(URL)
-    .then(() => console.log("Self ping success"))
-    .catch((err) => console.log("Self ping failed", err.message));
-}, 420000);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
